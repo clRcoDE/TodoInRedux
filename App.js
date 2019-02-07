29e6185602c7b407a943da5a06ed265c3331b8c6
clRcoDE <45456252+clRcoDE@users.noreply.github.com>
2019-02-07 09:58:46 +0330

    App.js is now deprecated

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow
+//  * @lint-ignore-every XPLATJSCOPYRIGHT1
+//  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-// import TodoApp from './src/Pages/TodoApp'
-// import MainTodo from './newsrc/MainTodo';
+// import React, {Component} from 'react';
+// import {Platform, StyleSheet, Text, View} from 'react-native';
+// // import TodoApp from './src/Pages/TodoApp'
+// // import MainTodo from './newsrc/MainTodo';
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-       {/* <TodoApp/> */}
-       {/* <MainTodo/> */}
-      </View>
-    );
-  }
-}
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//        {/* <TodoApp/> */}
+//        {/* <MainTodo/> */}
+//       </View>
+//     );
+//   }
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    // alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     // alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+// });
